a10e1a48f126ffa321f1a9199f4005efe21d2762
DanielCs1609 <dcsilva1609@gmail.com>
2024-05-23 22:46:11 -0300

    Projeto Book

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,15 +8,9 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
+ >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Olá</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
